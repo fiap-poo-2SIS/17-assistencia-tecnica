2ee99fd094a3a1aa1ea180810d80bda05e34525a
Antonio Marcos Selmini <marcos.selmini@uol.com.br>
2025-10-15 22:46:43 -0300

    Cliente atualizado e funcionando

---
+++ src/main/java/br/fiap/assistencia_tecnica/service/ClienteService.java
@@ -0,0 +1,24 @@
+package br.fiap.assistencia_tecnica.service;
+
+import br.fiap.assistencia_tecnica.domain.Cliente;
+import br.fiap.assistencia_tecnica.repository.ClienteRepository;
+import br.fiap.assistencia_tecnica.web.dto.ClienteDTO;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ClienteService {
+    private ClienteRepository clienteRepository;
+
+    public ClienteService(ClienteRepository clienteRepository) {
+        this.clienteRepository = clienteRepository;
+    }
+    public Cliente salvar(ClienteDTO clienteDTO){
+        Cliente cliente = new Cliente();
+        cliente.setNome(clienteDTO.getNome());
+        cliente.setEmail(clienteDTO.getEmail());
+        cliente.setSenha(clienteDTO.getSenha());
+        cliente.setTelefone(clienteDTO.getTelefone());
+        //return clienteRepository.save(cliente);
+        clienteRepository.fin
+    }
+}
